766150a95b10a84ada29590ed46e5082a5248f69
Em Coraccio <emcoraccio@gmail.com>
2019-10-27 13:14:29 -0400

    created models

---
+++ models/Article.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+let ArticleSchema = new Schema({
+
+  title: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String, 
+    required: true
+  },
+  summary: {
+    type: String
+  },
+  category: {
+    type: String
+  },
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
+});
+
+const Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
